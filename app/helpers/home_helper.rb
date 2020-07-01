89d47b4bcb3e0a167491275c67cd55d5604e7aba
dev-julien-hub <julien_42@sayna.io>
2020-07-01 14:20:39 +0300

    back

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,28 @@
 module HomeHelper
 
 	def JsonProjectsGet()
+		@value =[]
 		@All_projets = Projet.all
-		return @All_projets
+		for @one_projets in @All_projets
+			@user= User.find(@one_projets.user_id)
+			@value << {
+				"name" => @user.name,
+				"prenom" => @user.last_name,
+				"sexe" => @user.sexe,
+				"pays" => @user.pays,
+				"titre" => @one_projets.titre_du_projets,
+				"description" => @one_projets.description_du_projets,
+				"categorie" => @one_projets.categorie_du_projets,
+				"localisation" => @one_projets.localisation_du_projets,
+				"jeutons" => @one_projets.jeutons_du_projets,
+				"valifunders" => @one_projets.valifunders,
+				"jours" => @one_projets.nombre_de_days,
+				"fin" => @one_projets.fin_du_projets,
+				"pourcentage" => @one_projets.pourcentage,
+				"like" => @one_projets.nombre_de_like
+			}
+		end
+		return @value
 	end
 
 end
